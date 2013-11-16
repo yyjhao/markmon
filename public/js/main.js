5fc3f2fb4d3c02dcc9013eeb2ffb616a10e4c5cf
yyjhao <yyjhao@gmail.com>
2013-11-16 15:39:58 +0800

    animate scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,36 @@
 (function(markmon){
+    Math.easeOutCirc = function (t, b, c, d) {
+        t /= d;
+        t--;
+        return c * Math.sqrt(1 - t*t) + b;
+    };
+
+    var scroller = markmon.scroller = {
+        scrollTimer: 0,
+        stepTime: 10,
+        scorllTo: function(y){
+            clearTimeout(this.scrollTimer);
+            if(y === window.scrollY) return;
+            this.time = 200;
+            this.tick = (y - window.scrollY) / this.time * 10;
+            this.step = 0;
+            this.allSteps = this.time / 10;
+            this.ori = window.scrollY;
+            this.des = y;
+            this.scroll();
+        },
+        scroll: function(){
+            this.step++;
+            var pos = Math.easeOutCirc(this.step, this.ori, this.step * this.tick, this.allSteps);
+            window.scrollTo(0, pos | 0);
+            if(Math.abs(this.des - pos) > 2) {
+                this.scrollTimer = setTimeout(this.scroll, 10);
+            }
+        }
+    };
+
+    scroller.scroll = scroller.scroll.bind(scroller);
+
     var highlighter = document.querySelector(".highlighter");
     var socket = io.connect(location.origin);
     socket.on("content", function(data){
@@ -17,7 +49,7 @@
                 setTimeout(function(){
                     var line = r.lastChange.offsetTop + r.lastChange.offsetHeight;
                     highlighter.style.top = line + "px";
-                    window.scrollTo(0, line - window.innerHeight / 2 |0);
+                    scroller.scorllTo(line - window.innerHeight / 2 | 0);
                 }, 10);
             }
         });
